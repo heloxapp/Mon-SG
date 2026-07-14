e8927af9054bd1754e7efd179b4161f5eb9605b7
heloxapp <heloxfortnite@gmail.com>
2026-07-14 15:54:05 +0200

    fix: clean base path

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   // ATTENTION : Remplace 'nom-de-ton-depot' par le nom exact de ton projet sur GitHub !
   // Par exemple : si ton dépôt GitHub s'appelle "ma-super-pwa", écris : base: '/ma-super-pwa/',
-base: '/Mon-SG/',
+ base: '/Mon-SG/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
